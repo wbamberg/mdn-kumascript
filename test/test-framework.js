ea55c8e857433347bade172be9e48b9d20d9b82c
wbamberg <will@bootbonnet.ca>
2023-07-14 20:36:37 -0700

    Add front matter update test

--- test/test-framework.js
+++ test/test-framework.js
@@ -2,10 +2,19 @@ import assert from "assert";
 import { processMacros } from "../index.js";
 
 const input = `<p>The {{cssxref("margin")}} property and the {{experimental_inline}} and also the {{cssref}} thing, and also the {{embedinteractiveexample("the-link")}}, and even the {{cssxref("border", "border syntax", "#syntax)}}</p>`;
+const expectedMarkup = `<p>The <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/margin"><code>margin</code></a> property and the <abbr class="icon icon-experimental" title="Experimental. Expect behavior to change in the future."><span class="visually-hidden">Experimental</span></abbr> and also the  thing, and also the , and even the <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border#syntax"><code>border syntax</code></a></p>`;
 
-const expected = `<p>The <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/margin"><code>margin</code></a> property and the <abbr class="icon icon-experimental" title="Experimental. Expect behavior to change in the future."><span class="visually-hidden">Experimental</span></abbr> and also the  thing, and also the , and even the <a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border#syntax"><code>border syntax</code></a></p>`;
+const expectedFrontMatter = {
+  "interactive-example":
+    "https://interactive-examples.mdn.mozilla.net/the-link",
+};
 
 describe("Macro unit tests", () => {
   const output = processMacros(input, {});
-  it("processes macros", () => assert.equal(output.markup, expected));
+  it("updates markup", () => assert.equal(output.markup, expectedMarkup));
+  it("updates front matter", () =>
+    assert.equal(
+      output.frontMatter["interactive-example"],
+      expectedFrontMatter["interactive-example"]
+    ));
 });
