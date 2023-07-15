f051b5148c2e79497ee178f34c8c2db77aece1d2
wbamberg <will@bootbonnet.ca>
2023-07-14 18:29:44 -0700

    More tests

--- test/test-macros.js
+++ test/test-macros.js
@@ -3,68 +3,110 @@ import assert from "assert";
 import { processors } from "../processors.js";
 
 const expected = {
-  cssref: "",
   cssxref: {
     "special-case-color":
       '<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/color_value"><code>&lt;color&gt;</code></a>',
-    "data-type-argument": "",
-    "function-with-brackets": "",
-    "function-without-brackets": "",
-    "data-type-with-brackets": "",
-    "data-type-without-brackets": "",
-    "custom-display-name": "",
-    fragment: "",
+    "add-brackets-to-function":
+      '<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/max"><code>max()</code></a>',
+    "add-brackets-to-data-type":
+      '<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/angle"><code>&lt;angle&gt;</code></a>',
+    "custom-display-name":
+      '<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/color"><code>this color</code></a>',
+    fragment:
+      '<a href="https://developer.mozilla.org/en-US/docs/Web/CSS/border#syntax"><code>border</code></a>',
   },
-  deprecated_header: "",
-  embedinteractiveexample: "",
   experimental_inline:
     '<abbr class="icon icon-experimental" title="Experimental. Expect behavior to change in the future."><span class="visually-hidden">Experimental</span></abbr>',
-  "non-standard_header": "",
-  seecompattable: "",
   "non-standard_inline":
     '<abbr class="icon icon-nonstandard" title="Non-standard. Check cross-browser support before using."><span class="visually-hidden">Non-standard</span></abbr>',
 };
 
-/*
-export const processors = {
-  cssxref: cssxrefProcessor,
-  "non-standard_inline": nonStandardInlineProcessor,
-};
-*/
-
 describe("Macro unit tests", () => {
-  it("Testing macro: cssxref", () =>
-    assert.equal(
-      processors["cssxref"](["&lt;color&gt;"], {
-        "page-type": "css-data-type",
-      }),
-      expected["cssxref"]["special-case-color"]
-    ));
-  it("Testing macro: deprecated_header", () =>
-    assert.equal(
-      processors["deprecated_header"](),
-      expected["deprecated_header"]
-    ));
-  it("Testing macro: embedinteractiveexample", () => {
-    const frontMatter = {};
-    const result = processors["embedinteractiveexample"](
-      ["thing"],
-      frontMatter
-    );
-    assert.equal(result, expected["embedinteractiveexample"]);
-    assert.equal(
-      frontMatter["interactive-example"],
-      "https://interactive-examples.mdn.mozilla.net/thing"
-    );
+  // cssref
+  describe("cssref", () => {
+    it("strips macro", () => assert.equal(processors["cssref"](), ""));
   });
-  it("Testing macro: experimental_inline", () =>
-    assert.equal(
-      processors["experimental_inline"](),
-      expected["experimental_inline"]
-    ));
-  it("Testing macro: non-standard_inline", () =>
-    assert.equal(
-      processors["non-standard_inline"](),
-      expected["non-standard_inline"]
-    ));
+  // cssxref
+  describe("cssxref", () => {
+    it("special-cases <color> type", () =>
+      assert.equal(
+        processors["cssxref"](["&lt;color&gt;"], {
+          "page-type": "css-type",
+        }),
+        expected["cssxref"]["special-case-color"]
+      ));
+    it("adds brackets to functions", () =>
+      assert.equal(
+        processors["cssxref"](["max"], {
+          "page-type": "css-function",
+        }),
+        expected["cssxref"]["add-brackets-to-function"]
+      ));
+    it("adds brackets to data types", () =>
+      assert.equal(
+        processors["cssxref"](["angle"], {
+          "page-type": "css-type",
+        }),
+        expected["cssxref"]["add-brackets-to-data-type"]
+      ));
+    it("supports custom display names", () =>
+      assert.equal(
+        processors["cssxref"](["color", "this color"], {
+          "page-type": "css-property",
+        }),
+        expected["cssxref"]["custom-display-name"]
+      ));
+    it("supports fragments", () =>
+      assert.equal(
+        processors["cssxref"](["border", "", "#syntax"], {
+          "page-type": "css-property",
+        }),
+        expected["cssxref"]["fragment"]
+      ));
+  });
+  // deprecated_header
+  describe("deprecated_header", () => {
+    it("strips macro", () =>
+      assert.equal(processors["deprecated_header"](), ""));
+  });
+  // embedinteractiveexample
+  describe("embedinteractiveexample", () => {
+    it("strips macro, appends to front matter", () => {
+      const frontMatter = {};
+      const result = processors["embedinteractiveexample"](
+        ["thing"],
+        frontMatter
+      );
+      assert.equal(result, "");
+      assert.equal(
+        frontMatter["interactive-example"],
+        "https://interactive-examples.mdn.mozilla.net/thing"
+      );
+    });
+  });
+  // experimental_inline
+  describe("experimental_inline", () => {
+    it("generates markup", () =>
+      assert.equal(
+        processors["experimental_inline"](),
+        expected["experimental_inline"]
+      ));
+  });
+  // non-standard_header
+  describe("non-standard_header", () => {
+    it("strips macro", () =>
+      assert.equal(processors["non-standard_header"](), ""));
+  });
+  // non-standard_inline
+  describe("non-standard_inline", () => {
+    it("generates markup", () =>
+      assert.equal(
+        processors["non-standard_inline"](),
+        expected["non-standard_inline"]
+      ));
+  });
+  // seecompattable
+  describe("seecompattable", () => {
+    it("strips macro", () => assert.equal(processors["seecompattable"](), ""));
+  });
 });
