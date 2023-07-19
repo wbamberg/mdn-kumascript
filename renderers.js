d76cdbe66a887c7c4cbf34e88ac8135cf2a2fd43
wbamberg <will@bootbonnet.ca>
2023-07-19 10:17:15 -0700

    consistent case for renderers

--- renderers.js
+++ renderers.js
@@ -1,13 +1,13 @@
 import { renderMacro as cssxrefRenderer } from "./macros/cssxref.js";
-import { renderMacro as deprecatedInlineRenderer } from "./macros/deprecated_inline.js";
+import { renderMacro as deprecatedinlineRenderer } from "./macros/deprecated_inline.js";
 import { renderMacro as domxrefRenderer } from "./macros/domxref.js";
-import { renderMacro as embedInteractiveExampleRenderer } from "./macros/embedinteractiveexample.js";
-import { renderMacro as experimentalInlineRenderer } from "./macros/experimental_inline.js";
+import { renderMacro as embedinteractiveexampleRenderer } from "./macros/embedinteractiveexample.js";
+import { renderMacro as experimentalinlineRenderer } from "./macros/experimental_inline.js";
 import { renderMacro as glossaryRenderer } from "./macros/glossary.js";
 import { renderMacro as htmlelementRenderer } from "./macros/htmlelement.js";
 import { renderMacro as httpheaderRenderer } from "./macros/httpheader.js";
-import { renderMacro as nonStandardInlineRenderer } from "./macros/non-standard_inline.js";
-import { renderMacro as optionalInlineRenderer } from "./macros/optional_inline.js";
+import { renderMacro as nonstandardinlineRenderer } from "./macros/non-standard_inline.js";
+import { renderMacro as optionalinlineRenderer } from "./macros/optional_inline.js";
 import { renderMacro as rfcRenderer } from "./macros/rfc.js";
 import { renderMacro as svgattrRenderer } from "./macros/svgattr.js";
 import { renderMacro as svgelementRenderer } from "./macros/svgelement.js";
@@ -23,16 +23,16 @@ export const renderers = {
   cssxref: cssxrefRenderer,
   csssyntax: stripMacro,
   deprecated_header: stripMacro,
-  deprecated_inline: deprecatedInlineRenderer,
+  deprecated_inline: deprecatedinlineRenderer,
   domxref: domxrefRenderer,
-  embedinteractiveexample: embedInteractiveExampleRenderer,
-  experimental_inline: experimentalInlineRenderer,
+  embedinteractiveexample: embedinteractiveexampleRenderer,
+  experimental_inline: experimentalinlineRenderer,
   glossary: glossaryRenderer,
   htmlelement: htmlelementRenderer,
   httpheader: httpheaderRenderer,
   "non-standard_header": stripMacro,
-  "non-standard_inline": nonStandardInlineRenderer,
-  optional_inline: optionalInlineRenderer,
+  "non-standard_inline": nonstandardinlineRenderer,
+  optional_inline: optionalinlineRenderer,
   rfc: rfcRenderer,
   seecompattable: stripMacro,
   specifications: stripMacro,
